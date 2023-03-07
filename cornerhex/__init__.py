88dae418c033bbe125822cc964f927d3ea539556
Sebastian Stammler <sebastian.stammler@gmail.com>
2023-03-07 12:25:11 +0100

    Update to version 0.2.0

--- cornerhex/__init__.py
+++ cornerhex/__init__.py
@@ -2,7 +2,7 @@ from cornerhex.plot import cornerplot
 from cornerhex.plot import sigma_to_quantile
 
 __name__ = "cornerhex"
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 
 __all__ = [
     "cornerplot",
